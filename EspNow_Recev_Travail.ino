a2f0ea84658461603055d5c5a0ea93b93ce2d69f
DanielsEsp <daniel.esp.iot@gmail.com>
2026-01-12 17:04:27 +0100

    Add files via upload

---
+++ EspNow_Recev_Travail.ino
@@ -0,0 +1,65 @@
+
+
+
+
+//        MAC Address 48:55:19:16:57:C5
+
+
+#include <ESP8266WiFi.h>
+extern "C" {
+  #include <espnow.h>
+}
+
+// ---- STRUCTURE DATAS ESPNOW ----
+typedef struct {
+                float temp;
+                float batt;
+               } espnowData;
+
+                 espnowData rxData;
+
+
+// -------- CALLBACK ESPNOW --------
+void onDataRecv (uint8_t *mac, uint8_t *incomingData, uint8_t len) {
+  
+     if (len == sizeof(espnowData)) {
+         memcpy(&rxData, incomingData, sizeof(rxData));
+
+//       Envoi vers ESP32 (Socket)
+         Serial.print("T=");
+         Serial.print(rxData.temp, 1);
+         Serial.print(";B=");
+         Serial.println(rxData.batt, 2);
+        }
+}
+
+
+//   -------- SETUP --------
+
+void setup() {
+  
+     Serial.begin(115200);
+
+     WiFi.mode(WIFI_STA);
+     WiFi.disconnect();   // nécessaire pour ESPNOW sur ESP8266
+
+     if (esp_now_init() != 0) {
+     //  échec init ESPNOW
+         return;
+        }
+
+     esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+     esp_now_register_recv_cb(onDataRecv);
+}
+
+
+//   -------- LOOP --------
+
+void loop() {
+  
+//   Boucle vide
+  
+}
+
+
+//
---
+++ EspNow_Send_Travail.ino
@@ -0,0 +1,90 @@
+
+
+
+
+#include <ESP8266WiFi.h>
+extern "C" {
+  #include <espnow.h>
+}
+uint8_t receiverMac[] = {0x48,0x55,0x19,0x16,0x57,0xC5};
+
+// -------- STRUCTURE ESPNOW --------
+typedef struct {
+                float temp;
+                float batt;
+               } espnowData;
+
+                 espnowData txData;
+                 
+// ------ CONFIGURATION DUREE SOMMEIL -------
+#define SLEEP_TIME_US 300e6   // 5 minutes
+//
+#include <Wire.h>
+// ------ CONFIGURATION CAPTEUR TEMPERATURE ------
+#include <Adafruit_Sensor.h>
+#include "Adafruit_Si7021.h"
+          Adafruit_Si7021 sensor = Adafruit_Si7021();
+// ------ CONFIGURATION CIRCUIT MESURE TENSION ------
+#include "Adafruit_ADS1X15.h"
+          Adafruit_ADS1115 ads;
+          
+
+//   -------- SETUP --------
+void setup() {
+  
+     Serial.begin(115200); Serial.println(); Serial.println();
+     
+     Wire.begin(); //  I2C = 0x40 capteur température
+
+     ads.begin();  //  I2C = ADS1115 mesure tension
+     ads.setGain(GAIN_ONE); // ±4.096V → 0.125mV/bit
+
+     // --- WIFI / ESPNOW ---
+     WiFi.mode(WIFI_STA);
+     WiFi.disconnect();
+     if (esp_now_init() != 0) {
+         ESP.restart(); //     ESP RESTART si échec EspNow
+        }
+
+     esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+     esp_now_add_peer(receiverMac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+
+//   ------ MESURES TEMPERATURE ET TENSION BATTERIE ------
+     txData.temp = sensor.readTemperature();
+     Serial.print("\tTemperature: "); Serial.print(txData.temp);
+
+     float multivoltage = 0; //    Pour moyenne de 5 lectures tension batterie    
+     Serial.print("\tVoltage  : "); 
+     for (int boucle = 0; boucle < 5; boucle ++) {
+          int16_t raw = ads.readADC_SingleEnded(2);
+          float voltage = raw * 0.125 / 1000.0;   // ADS1115 → volts
+          Serial.print(voltage); Serial.print("-"); delay(100);
+          multivoltage = multivoltage + voltage;
+         }
+     multivoltage = multivoltage / 5; //  Tension de la batterie
+     Serial.print("\tMulti Voltage  : "); Serial.print(multivoltage);
+     multivoltage *= 1.028350515; //      Compense écart lecture et réel
+
+     txData.batt = multivoltage;
+     Serial.print("\tBatterie  : "); Serial.print(txData.batt);
+     Serial.println(); Serial.println();
+     
+//   ------ ENVOI ESPNOW ------
+     esp_now_send(receiverMac, (uint8_t*)&txData, sizeof(txData));
+     delay(50);  // temps radio
+
+//   ----- MISE EN SOMMEIL -----
+     ESP.deepSleep(SLEEP_TIME_US);
+}
+
+
+// -------- LOOP --------
+
+void loop() {
+  
+  // boucle vide
+  
+}
+
+
+//
